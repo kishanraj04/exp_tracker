47b6171c9983380b84059613dc910584b9f5c2d5
kishanraj04 <kishuraj1111@gmail.com>
2026-06-06 10:55:35 +0530

    delete expense controller

--- server/src/controllers/expenseController.js
+++ server/src/controllers/expenseController.js
@@ -54,3 +54,21 @@ export const updateExpense = async (req, res) => {
   }
 };
 
+export const deleteExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findByIdAndDelete(req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+
+    res.json({
+      message: "Expense deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
